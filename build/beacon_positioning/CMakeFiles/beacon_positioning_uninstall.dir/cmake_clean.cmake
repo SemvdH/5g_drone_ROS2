49e752d6b7c8efafe8a23b87a2ba8feed956b4ce
Sem van der Hoeven <semmer99@gmail.com>
2023-04-13 14:10:35 +0000

    add beacons positioning package

---
+++ build/beacon_positioning/CMakeFiles/beacon_positioning_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beacon_positioning_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beacon_positioning_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/beacon_positioning/src/tracker_position.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world beacon_positioning package\n");
+  return 0;
+}
