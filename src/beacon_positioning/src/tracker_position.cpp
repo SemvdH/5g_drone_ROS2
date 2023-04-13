2abed3f289d3c43e9e94dc2116feb971d0af14ea
Sem van der Hoeven <semmer99@gmail.com>
2023-04-13 17:22:28 +0200

    Add positioning driver API

--- src/beacon_positioning/src/tracker_position.cpp
+++ src/beacon_positioning/src/tracker_position.cpp
@@ -4,6 +4,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+#include "rtls_driver/rtls_driver.hpp"
+
 using namespace std::chrono_literals;
 
 class BeaconPositioningPublisher : public rclcpp::Node
