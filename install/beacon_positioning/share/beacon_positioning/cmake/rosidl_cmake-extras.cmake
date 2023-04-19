6b752142f7ae12b51200ae3a0702bdf7cfe70211
Sem van der Hoeven <semmer99@gmail.com>
2023-04-19 12:18:10 +0000

    remove build and install directory from tracking

--- install/beacon_positioning/share/beacon_positioning/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(beacon_positioning_IDL_FILES "msg/TrackerPosition.idl")
-set(beacon_positioning_INTERFACE_FILES "msg/TrackerPosition.msg")
