22126929a7ba5f3b97717ce4f3b6d62f81b0b76e
Sem van der Hoeven <semmer99@gmail.com>
2023-05-01 15:30:30 +0200

    remove placeholders

--- src/px4_connection/src/px4_controller.cpp
+++ src/px4_connection/src/px4_controller.cpp
@@ -38,7 +38,7 @@ public:
         trajectory_setpoint_publisher = this->create_publisher<px4_msgs::msg::TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
         // offboard_control_mode_publisher_ = this->create_publisher<px4_msgs::msg::OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
 
-        set_attitude_service_ = this->create_service<px4_connection::srv::SetAttitude>("set_attitude", std::bind(&PX4Controller::set_setpoint, this, _1, _2, _3));
+        set_attitude_service_ = this->create_service<px4_connection::srv::SetAttitude>("set_attitude", std::bind(&PX4Controller::set_setpoint, this));
 
         //         service_ptr_ = this->create_service<std_srvs::srv::Empty>(
         //         "test_service",
