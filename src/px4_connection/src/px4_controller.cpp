9c3dcb463d4799654af57584bf72643e07d67cf6
Sem van der Hoeven <semmer99@gmail.com>
2023-05-01 11:23:25 +0200

    make dat boi schmoov

--- src/px4_connection/src/px4_controller.cpp
+++ src/px4_connection/src/px4_controller.cpp
@@ -62,7 +62,7 @@ private:
 
         if (setpoint_count < 30)
         {
-            msg.velocity[0] = 0;
+            msg.velocity[0] = 5;
             msg.velocity[1] = 1;
             msg.velocity[2] = D_SPEED(10);
             msg.yawspeed = 0;
