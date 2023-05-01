eeb67733d3f33f15204999a429e5a6638ae59fd2
ubuntu <ubuntu@ubuntu-20-drone.myguest.virtualbox.org>
2023-05-01 13:49:31 +0200

    create drone_controls package

--- get_build.sh
+++ get_build.sh
@@ -3,5 +3,5 @@ echo "updating git..."
 ./fgit.sh
 echo "building package..."
 colcon build --packages-select px4_connection
-echo "launching controller..."
-ros2 run px4_connection px4_controller
+#echo "launching controller..."
+#ros2 run px4_connection px4_controller
