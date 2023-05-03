e5ae3dedfb0c1c5d06c957a7fe8184f243457a19
Sem van der Hoeven <semmer99@gmail.com>
2023-05-03 14:02:01 +0200

    add package description and licence

--- src/test_controls/setup.py
+++ src/test_controls/setup.py
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='ubuntu',
     maintainer_email='semmer99@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='test controls',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
