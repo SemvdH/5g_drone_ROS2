93b09a49a9eda089b19bc409b6020fe0873fefbe
Sem van der Hoeven <semmer99@gmail.com>
2023-05-08 17:17:58 +0200

    add formatting image name

--- src/camera/camera/camera_controller.py
+++ src/camera/camera/camera_controller.py
@@ -4,13 +4,14 @@ from rclpy.node import Node
 from drone_services.srv import TakePicture
 
 import cv2
+from datetime import datetime
 
 
 class CameraController(Node):
     def __init__(self):
         super().__init__('camera_controller')
         self.cam = cv2.VideoCapture(0)
-
+        self.get_logger().info("Camera controller started. Waiting for service call...")
         self.srv = self.create_service(TakePicture, 'drone/picture', self.take_picture_callback)
 
     def take_picture_callback(self, request, response):
@@ -18,11 +19,14 @@ class CameraController(Node):
         if (result):
             if (request.input_name == "default"):
                 self.get_logger().info("Taking picture with default filename")
-                cv2.imwrite("/home/ubuntu/image.jpg", image)
-                response.filename = "/home/ubuntu/image.jpg"
+                now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                imagename = "/home/ubuntu/" + now + ".jpg"
+                cv2.imwrite(imagename, image)
+                response.filename = imagename
             else:
                 cv2.imwrite(request.input_name, image)
                 response.filename = request.input_name
+            self.get_logger().info("Picture saved as " + response.filename)
             return response
 
 def main(args=None):
