e16e083dd0b89491bfb2e37280ca9c02f543abec
Sem van der Hoeven <semmer99@gmail.com>
2023-05-17 11:31:22 +0200

    add service for controlling relais

--- src/relais_control/relais_control/relais_controller.py
+++ src/relais_control/relais_control/relais_controller.py
@@ -1,13 +1,17 @@
 import rclpy
 from rclpy.node import Node
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
+from drone_services.srv import ControlRelais
 class RelaisController(Node):
     def __init__(self):
         super().__init__('relais_controller')
+        self.srv = self.create_service(ControlRelais, 'control_relais', self.control_relais_callback)
+
         self.relais1_pin = 17
         self.relais2_pin = 27
         self.init_gpio()
@@ -29,6 +33,26 @@ class RelaisController(Node):
         GPIO.output(self.relais2_pin, GPIO.HIGH)
         self.get_logger().info("Relais turned on")
 
+    def control_relais_callback(self, request, response):
+        if request.relais1:
+            GPIO.output(self.relais1_pin, GPIO.HIGH)
+        else:
+            GPIO.output(self.relais1_pin, GPIO.LOW)
+        if request.relais2:
+            GPIO.output(self.relais2_pin, GPIO.HIGH)
+        else:
+            GPIO.output(self.relais2_pin, GPIO.LOW)
+        
+        if GPIO.output(17) == GPIO.LOW:
+            response.bits = response.bits & 0
+        else:
+            response.bits = response.bits | 1
+
+        if GPIO.output(27) == GPIO.LOW:
+            response.bits = response.bits & ~(1 << 1)
+        else:
+            response.bits = response.bits | (1 << 1)
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
