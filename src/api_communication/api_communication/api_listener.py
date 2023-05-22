1b5a2f9c3a90c8f828b1701c99333abdf1b101ea
Sem van der Hoeven <semmer99@gmail.com>
2023-05-22 15:54:03 +0200

    add thread that runs asyncio daemon

--- src/api_communication/api_communication/api_listener.py
+++ src/api_communication/api_communication/api_listener.py
@@ -16,16 +16,16 @@ class ApiListener(Node):
         self.last_battery_percentage = 0
         self.last_cpu_usage = 0
         self.server = None
-        self.server_thread = threading.Thread(target=self.handle_api,daemon=True)
+        self.server_thread = threading.Thread(target=self.start_api_thread,daemon=True)
+        self.server_thread.start()
     
     def drone_status_callback(self, msg):
         self.get_logger().info('Received drone battery and cpu: {0} {1}'.format(msg.battery_percentage,msg.cpu_usage))
         self.last_battery_percentage = msg.battery_percentage
         self.last_cpu_usage = msg.cpu_usage
     
-    def spin(self):
-        while rclpy.ok():
-            rclpy.spin_once(self,timeout_sec=0.1)
+    def start_api_thread(self):
+        asyncio.run(self.handle_api())
 
     async def handle_api(self):
         self.get_logger().info('Starting API')
@@ -44,17 +44,15 @@ class ApiListener(Node):
             self.get_logger().info('Connection closed')
 
 
-async def main(args=None):
+def main(args=None):
     rclpy.init(args=args)
 
     api_listener = ApiListener()
-    
+
     rclpy.spin(api_listener)
 
     api_listener.destroy_node()
     rclpy.shutdown()
 
-print("running main")
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.run_forever()
+if __name__ == '__main__':
+    main()
