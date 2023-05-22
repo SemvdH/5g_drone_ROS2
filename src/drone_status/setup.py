0ceb7ac098d86dae0e31c4029b5292fe320dd05e
Sem van der Hoeven <semmer99@gmail.com>
2023-05-22 14:13:38 +0200

    typo

--- src/drone_status/setup.py
+++ src/drone_status/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                    'drone_status = drone_status.drone_status:main's
+                    'drone_status = drone_status.drone_status:main'
         ],
     },
 )
