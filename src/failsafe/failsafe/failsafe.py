b779dba01aaf0fc003a49d50079eb3eb0826d73c
Sem van der Hoeven <semmer99@gmail.com>
2023-05-26 21:38:58 +0200

    add failsafe node

--- src/failsafe/failsafe/failsafe.py
+++ src/failsafe/failsafe/failsafe.py
@@ -1,9 +1,34 @@
 import rclpy
 from rclpy.node import Node
 
+from drone_services.srv import EnableFailsafe
+from drone_services.msg import FailsafeMsg
 class FailSafe(Node):
     def __init__(self):
         super().init("failsafe")
+        self.failsafe_enabled = False
+        self.failsafe_msg = ""
+        self.get_logger().info("Failsafe node started")
+        # create service on /drone/failsafe topic
+        self.service = self.create_service(
+            EnableFailsafe, "/drone/enable_failsafe", self.failsafe_callback)
+        self.failsafe_publisher = self.create_publisher(FailsafeMsg, "/drone/failsafe", 10)
+    
+    def failsafe_callback(self, request, response):
+        self.failsafe_enabled = True
+        self.failsafe_msg = request.message
+        response.enabled = self.failsafe_enabled
+        response.message = self.failsafe_msg
+        self.get_logger().info("Failsafe triggered")
+        self.publish_failsafe()
+        return response
+    
+    def publish_failsafe(self):
+        msg = FailsafeMsg()
+        msg.enabled = self.failsafe_enabled
+        msg.message = self.failsafe_msg
+        self.failsafe_publisher.publish(msg)
+        self.get_logger().info("Publishing failsafe message")
 
 
 def main(args=None):
